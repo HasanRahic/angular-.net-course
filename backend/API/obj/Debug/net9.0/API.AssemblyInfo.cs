6e96aa2fffc6acaa4ffbd9b7e93d7d736125d246
Hasan Rahic <haso.raha10@gmail.com>
2025-05-17 10:56:08 +0200

    End of section 12 - Reactive forms

--- backend/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ backend/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8af401cfd83bd4f1f88405447bda7f793a0803f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baf94497cc6a45aea7ac6cdbe4e676ce89d0c1a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
